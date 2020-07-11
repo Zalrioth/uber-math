36de9d838ddb03d2b7de0724ed5ed49b3b78b932
Zalrioth <nicholasbedner@gmail.com>
2020-07-10 18:46:43 -0700

    Ivec3

--- include/ubermath/ivec3.h
+++ include/ubermath/ivec3.h
@@ -16,34 +16,34 @@ typedef struct ivec3 {
   };
 } ivec3;
 
-static inline ivec3 vec3_add(ivec3 a, ivec3 b);
-static inline ivec3 vec3_sub(ivec3 a, ivec3 b);
-static inline ivec3 vec3_mul(ivec3 a, ivec3 b);
-static inline ivec3 vec3_div(ivec3 a, ivec3 b);
-static inline ivec3 vec3_divs(ivec3 a, int s);
-static inline ivec3 vec3_scale(ivec3 a, int s);
+static inline ivec3 ivec3_add(ivec3 a, ivec3 b);
+static inline ivec3 ivec3_sub(ivec3 a, ivec3 b);
+static inline ivec3 ivec3_mul(ivec3 a, ivec3 b);
+static inline ivec3 ivec3_div(ivec3 a, ivec3 b);
+static inline ivec3 ivec3_divs(ivec3 a, int s);
+static inline ivec3 ivec3_scale(ivec3 a, int s);
 
-static inline ivec3 vec3_add(ivec3 a, ivec3 b) {
+static inline ivec3 ivec3_add(ivec3 a, ivec3 b) {
   return (ivec3){.data[0] = a.data[0] + b.data[0], .data[1] = a.data[1] + b.data[1], .data[2] = a.data[2] + b.data[2]};
 }
 
-static inline ivec3 vec3_sub(ivec3 a, ivec3 b) {
+static inline ivec3 ivec3_sub(ivec3 a, ivec3 b) {
   return (ivec3){.data[0] = a.data[0] - b.data[0], .data[1] = a.data[1] - b.data[1], .data[2] = a.data[2] - b.data[2]};
 }
 
-static inline ivec3 vec3_mul(ivec3 a, ivec3 b) {
+static inline ivec3 ivec3_mul(ivec3 a, ivec3 b) {
   return (ivec3){.data[0] = a.data[0] * b.data[0], .data[1] = a.data[1] * b.data[1], .data[2] = a.data[2] * b.data[2]};
 }
 
-static inline ivec3 vec3_div(ivec3 a, ivec3 b) {
+static inline ivec3 ivec3_div(ivec3 a, ivec3 b) {
   return (ivec3){.data[0] = a.data[0] / b.data[0], .data[1] = a.data[1] / b.data[1], .data[2] = a.data[2] / b.data[2]};
 }
 
-static inline ivec3 vec3_divs(ivec3 a, int s) {
+static inline ivec3 ivec3_divs(ivec3 a, int s) {
   return (ivec3){.data[0] = a.data[0] / s, .data[1] = a.data[1] / s, .data[2] = a.data[2] / s};
 }
 
-static inline ivec3 vec3_scale(ivec3 a, int s) {
+static inline ivec3 ivec3_scale(ivec3 a, int s) {
   return (ivec3){.data[0] = a.data[0] * s, .data[1] = a.data[1] * s, .data[2] = a.data[2] * s};
 }
 
